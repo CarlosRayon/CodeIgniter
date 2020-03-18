e3f6b85710f6a345c6c4e60696ba5a4d443f94fa
Carlos Rayon <carlosrayon.developer@gmail.com>
2020-03-18 22:16:56 +0100

    first commit

---
+++ application/views/home.php
@@ -0,0 +1,29 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Templete Codeigniter">
+    <meta name="keywords" content="codeigniter, carlos rayón, templete" />
+    <meta name="author" content="Carlos Rayón" >
+    <meta name="copyright" content="copyright" />
+    <title>Home</title>
+    <link rel="stylesheet" href="<?php echo base_url('assets/css/main.css') ?>">
+</head>
+
+<body>
+
+    <main class="bg">
+        <h1>HELLO WORLD</h1>
+    </main>
+
+    <!-- scripss js -->
+    <script src="<?php echo base_url('assets/js/main.js') ?>"></script>
+</body>
+
+</html>
